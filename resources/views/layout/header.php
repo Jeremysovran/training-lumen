2b49cd178c7a862c1bd3fa17955b934a06d4e8b2
Jeremy Sovran <jeremysovran@gmail.com>
2018-11-20 17:04:51 +0100

    header route OK

--- resources/views/layout/header.php
+++ resources/views/layout/header.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="/training/traning-lumen/public/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/training/training-lumen/public/assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-<link rel="stylesheet" href="/training/traning-lumen/public/assets/css/styles.css">
+<link rel="stylesheet" href="/training/training-lumen/public/assets/css/styles.css">
 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>projet training</title>
@@ -19,11 +19,11 @@
         <div class="container-fluid">
             <div class="row topBar pt-2 pb-2 d-lg-none">
                 <div class="col-3"><i class="fas fa-arrow-left"></i></div>
-                <div class="col-9"><?= $users->name ?></div>
+                <div class="col-9"></div>
                 
             </div>
             <div class="row topBar p-2 d-none d-lg-flex justify-content-between topFixed">
-            <div class=""><?= $users->name ?></div>
+            <div class=""></div>
             <div><a href="" class="text-light">deconnexion</a></div>
             </div>
         </div>
